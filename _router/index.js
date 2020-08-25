cb24c17be4b49ef09bb6c45d038ab169a5a4a680
Faiq Allam <faiq.kaboel@gmail.com>
2020-08-25 07:57:40 +0700

    register transaction router

--- _router/index.js
+++ _router/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 router.use('/user', require('./modules/user'));
 router.use('/menu', require('./modules/menu'));
+router.use('/transaction', require('./modules/transaction'));
 
 module.exports = router;
