5965d19ef0e025ab4c6b0e74416a7c2a56ee8d8e
Faiq Allam <faiq.kaboel@gmail.com>
2020-08-25 15:08:15 +0700

    register getTransactionDetail  handler method

--- _router/modules/transaction.js
+++ _router/modules/transaction.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const {
-  tokenVerify
+  tokenVerify,
 } = require('../../_middleware/Auth');
-const { omitTransaction } = require('../../_controller/TransactionController');
+const {
+  omitTransaction,
+  getTransactionDetail,
+} = require('../../_controller/TransactionController');
 
 router.post('/make', tokenVerify, omitTransaction)
+router.get('/preview/:id', tokenVerify, getTransactionDetail)
 
 module.exports = router;
